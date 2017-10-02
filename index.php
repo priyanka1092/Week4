a43f39eaba83b9a11a03faf3b8bb3a5a944e032c
priyanka nilesh bongale <pb435@afsconnect1.njit.edu>
2017-10-01 21:36:29 -0400

    String Length

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$date =  date('Y-m-d', time());
+$date =  date('Y/m/d', time());
 echo "The value of \$date: ".$date."<br>";
 
 $tar = "2017/05/24";
@@ -7,6 +7,38 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
 
+//$replacedate = str_replace("-", "/", $date);
+//echo "The value of \$date:" .$replacedate. "<br>";
+
+echo '<hr>';
+
+if($date == $tar)
+{
+echo "Oops";
+}
+  else if($date > $tar)
+  {
+    echo "Future";
+  }  
+     else if($date < $tar)
+       {
+       	echo "Past";
+       }
+
+       echo '<hr>';
+       echo "The word count is".str_word_count("$date");
+          echo '<hr';
+
+          echo srtlen("$date");
+
+
+
+
+
+
+       
 ?>
+
+
